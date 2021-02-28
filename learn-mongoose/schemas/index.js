1db36da4353390d88f3e8ec07e82c27bf25d95e9
hayoung0Lee <hayoung0.lee@gmail.com>
2021-02-28 23:57:54 +0900

    Learn mongoose

---
+++ learn-mongoose/schemas/index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  if (process.env.NODE_ENV !== "production") {
+    mongoose.set("debug", true);
+  }
+  mongoose.connect(
+    "mongodb://admin:1234@localhost:27017/admin",
+    {
+      dbName: "nodejs",
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    },
+    (error) => {
+      if (error) {
+        console.log("mongodb 연결 에러", error);
+      } else {
+        console.log("mongo 디비 연결 성공");
+      }
+    }
+  );
+
+  mongoose.connection.on("error", (error) => {
+    console.error("몽고디비 연결 에러", error);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.error("연결이 끊어졌다");
+    connect();
+  });
+};
+
+module.exports = connect;
